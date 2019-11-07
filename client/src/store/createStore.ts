e3b497455598cff14f2bf8f2cd771cb14435369f
Исаева Мария <isaeva-mariya@yandex.ru>
2019-11-07 13:24:28 +0300

    added series in InitialState

--- client/src/store/createStore.ts
+++ client/src/store/createStore.ts
@@ -3,9 +3,11 @@ import {reducer} from './reducers';
 import thunk, {ThunkDispatch} from 'redux-thunk';
 import Trend from '../types/trend';
 import { items } from '../feed.json';
+import { items as seriesFeed } from '../mobile_series.json';
 
 export interface State {
     trends: Trend[];
+    [name: string]: any;
 }
 
 interface StoreExtension {
@@ -15,6 +17,7 @@ interface StoreExtension {
 const trends: any[] = items[0].includes;
 const initialState = {
     trends: trends.map(({ title, thumbnail }: any) => ({desc: title, img: thumbnail }) ),
+    series: seriesFeed,
 };
 const store = createStore<State, AnyAction, StoreExtension, void>(reducer, initialState, applyMiddleware(thunk));
 
