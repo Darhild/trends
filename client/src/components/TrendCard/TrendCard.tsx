636ffd79f965afaa14486794c8701ddce574233b
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-11-09 13:15:04 +0300

    add trend Card

--- client/src/components/TrendCard/TrendCard.tsx
+++ client/src/components/TrendCard/TrendCard.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classnames from 'classnames';
 import Trend from '../../types/trend';
+import { ReactComponent as Grow } from '../../images/svg/grow.svg';
 import './style.scss';
 
-const TrendCard = ({ img, desc }: Trend) =>
-    (
-        <div className="TrendCard" style={{ backgroundImage: `url(${img})` }}>
-            <p>{desc}</p>
-        </div>
-    );
+
+interface Props extends Trend {
+    ratingPosition: number;
+    className?: string;
+    variant?: string;
+}
+
+class TrendCard extends Component<Props> {
+    public render() {
+        const { img, desc, className, variant, ratingPosition } =  this.props;
+        const cn = classnames(
+            'TrendCard',
+            variant && `TrendCard_variant_${variant}`,
+            className,
+        );
+
+        return (
+            <div className={cn}>
+                <div className="TrendCard-Poster" style={{ backgroundImage: `url(${img})` }}>
+                </div>
+                <div className="TrendCard-Details TrendDetails">
+                    <div className="TrendDetails-Label"><Grow className="TrendDetails-Icon"/>
+                        <span>Сейчас популярно</span>
+                    </div>
+                    <div className="TrendDetails-Title">{desc}</div>
+                    <div className="TrendDetails-Count">123 видео и 2 тыс. историй</div>
+                </div>
+                <div className="TrendCard-Placeholder"></div>
+                <div className="TrendCard-Number">{ratingPosition}</div>
+            </div>
+        );
+    }
+
+}
 
 export default TrendCard;
