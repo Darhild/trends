da715fb78c5e3475c2973b30bbf6546d7ec59bad
Исаева Мария <isaeva-mariya@yandex.ru>
2019-11-11 15:42:57 +0300

    trenspage desktop version  and breakpoints

--- client/src/components/SmallCard/SmallCard.tsx
+++ client/src/components/SmallCard/SmallCard.tsx
@@ -3,12 +3,13 @@ import { ReactComponent as Grow } from '../../images/svg/grow.svg';
 import './SmallCard.scss';
 
 export interface SmallCardProps {
+    poster: string;
     img: string;
     desc: string;
 }
 
-const SmallCard = ({ img, desc }: SmallCardProps) => (
-    <div className="SmallCard" style={ { backgroundImage: `url(${img})` } }>
+const SmallCard = ({ img, desc, poster }: SmallCardProps) => (
+    <div className="SmallCard" style={ { backgroundImage: `url(${poster})` } }>
         <Grow className="SmallCard-Icon"/>
         <span className="SmallCard-Placeholder"></span>
         <p className="SmallCard-Footer">{desc}</p>
