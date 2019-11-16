d0edcedd6d02cda948b93ccdcccb25ac993832d8
Maria Isaeva <isaeva-mariya@yandex.ru>
2019-11-16 16:52:40 +0300

    tslint

--- client/src/pages/TrendsPage/TrendsPage.tsx
+++ client/src/pages/TrendsPage/TrendsPage.tsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import queryString from 'query-string';
+import { RouteComponentProps } from 'react-router';
 import Title from '../../components/Title/Title';
 import TrendsList from '../../components/TrendsList/TrendsList';
 import { State } from '../../store/createStore';
 
-interface TrendsPageProps {
+interface TrendsPageProps extends RouteComponentProps<TParam> {
     variant: string;
+}
+
+interface TParam {
+    category: string;
 }
 
 class TrendsPage extends Component<TrendsPageProps> {
     public render() {
+        const params = queryString.parse(this.props.location.search);
+        const { category } = this.props.match.params;
         const { variant } = this.props;
 
         return (
