ccd51b5cbad87ab48656e1b10a816d58665ae0b1
Oleg Pelipenko <oleg-fry@yandex-team.ru>
2019-11-16 16:31:28 +0300

    Merge pull request #72 in SHRI-2019/trenders from backend.api-fixes to master
    Squashed commit of the following:
    
    commit dc6c475fdcf0426e93e26ec99080ea23a170bb93
    Author: oleg.fry <oleg.fry@gmail.com>
    Date:   Sat Nov 16 16:18:34 2019 +0300
    
        Fixes for: https://tracker.yandex.ru/TRENDERS-45

---
+++ backend/web/trends/handlers/trends_algs.py
@@ -0,0 +1,51 @@
+
+def sort(result):
+    result_data = []
+    if result is None or len(result) == 0:
+        return result_data
+    # Сортируем по day
+    return sorted(result, key=lambda x: x["day"], reverse=True)
+
+
+def sort_and_limit(result, limit):
+    result_data = sort(result)
+
+    # Лимитируем по limit
+    result = result[:limit]
+
+    # Выбираем только "data"
+    for r in result:
+        result_data.append(r["data"])
+
+    return result_data
+
+
+def merge(src1, src2, factor1, factor2, limit):
+    src1_idx, src2_idx = 0, 0
+    result = []
+    counter = 1
+
+    while len(result) < limit:
+
+        if counter % factor1 == 0:
+            if src1_idx < len(src1):
+                result.append(src1[src1_idx])
+                src1_idx += 1
+            elif src2_idx < len(src2):
+                result.append(src2[src2_idx])
+                src2_idx += 1
+
+        if counter % factor2 == 0:
+            if src2_idx < len(src2):
+                result.append(src2[src2_idx])
+                src2_idx += 1
+            elif src1_idx < len(src1):
+                result.append(src1[src1_idx])
+                src1_idx += 1
+
+        counter += 1
+
+        if src1_idx >= len(src1) and src2_idx >= len(src2):
+            break
+
+    return result
