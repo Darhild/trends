0fc67acb8a7f414fd2c1b26ae3e7e07c0ac32fc5
Aleksandra Smirnova <smirnova-a-yu@yandex-team.ru>
2019-11-17 17:25:25 +0300

    Merge pull request #78 in SHRI-2019/trenders from frontend.express_proxy to master
    Squashed commit of the following:
    
    commit ca1a0be6246d4fbbe21bc047e9e8f4bfb29aa2f8
    Author: Alexander Brichencov <brichencov@yandex.ru>
    Date:   Sun Nov 17 12:59:33 2019 +0300
    
        add proxy to backend

--- client/index.js
+++ client/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const API_URL = 'http://84.201.160.40:8080';
+const axios = require('axios');
 
 app.use(express.static('build'));
+app.get('/api/*', (req, res) => {
+    axios.get(`${API_URL}${req.originalUrl}`)
+        .then((response) => {
+            res.json(response.data)
+        })
+        .catch(() => {
+            res.status(404)
+            res.send()
+        })
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build/index.html'))
