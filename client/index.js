8bd0c0ccdb113b8123a75da339711af517ac7018
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-11-19 14:30:43 +0300

    change backend api url

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const API_URL = 'http://84.201.160.40:8080';
+const API_URL = 'http://ether-backend.ru:8080';
 const axios = require('axios');
 
 app.use(express.static('build'));
