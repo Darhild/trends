c459b0ecce404024c5e8ccb34160137e3d294d23
Antonina Gerasiova <gerasiova-ant@yandex-team.ru>
2019-11-19 12:31:04 +0300

    Merge pull request #86 in SHRI-2019/trenders from backend.deploy_with_dockerhub to master
    Squashed commit of the following:
    
    commit 07f3f768134a3ddea36ce54a734165e95bfdcbb4
    Author: Antonina Gerasiova <antonina.gerasiova@yandex.ru>
    Date:   Tue Nov 19 12:21:41 2019 +0300
    
        New scripts for running services remotely

--- backend/content/run.sh
+++ backend/content/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 docker pull antoninagerasiova/trenders_efir:latest
-docker run -p8081:8080 -it --rm antoninagerasiova/trenders_efir:latest
+docker rm -f content || true
+docker run -p8081:8080 -d --rm --name content antoninagerasiova/trenders_efir:latest
 
--- backend/web/run.sh
+++ backend/web/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+docker rm -f web || true
 docker pull antoninagerasiova/trenders_mixer:latest
-docker run --network host -e DATABASE_URL=postgresql://me:hackme@127.0.0.1/trends -it --rm antoninagerasiova/trenders_mixer:latest
+docker run --network host -e DATABASE_URL=postgresql://me:hackme@127.0.0.1/trends -d --rm --name web antoninagerasiova/trenders_mixer:latest
 
