46ae025f17e3d5d2569541b35aacb6c1162dbfa8
Mariana Strix <marianastrix@gmail.com>
2019-11-22 20:59:10 +0300

    Added real data

--- backend/web/trends/collectors/videos.py
+++ backend/web/trends/collectors/videos.py
@@ -20,20 +20,15 @@ class VideosCollector(BaseCollector):
 
         try:
             # print("videos collect url {0}".format(self.source_link))
-            dir = os.path.dirname(os.path.dirname(__file__))
-            path_mock = os.path.join(dir, 'mock_videos.json')
-            with open(path_mock) as f:
-                resp = json.dumps(json.load(f))
+            response = requests.get(self.source_link)
+            if response.status_code == 200:
+                return self.insert_videos(response.content)
 
-            return self.insert_videos(resp)
-            # if response.status_code == 200:
-            #     return self.insert_content(response.content)
-            #
-            # if response.status_code == 202:
-            #     # Do nothing
-            #     return
-            # else:
-            #     raise Exception("videos returned status code: {0}".format(response.status_code))
+            if response.status_code == 202:
+                # Do nothing
+                return
+            else:
+                raise Exception("videos returned status code: {0}".format(response.status_code))
 
         except Exception as e:
             logging.getLogger(__name__).\
