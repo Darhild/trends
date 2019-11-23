28b7157dc96e59bb845539e98f56d263f625148c
Oleg Pelipenko <oleg-fry@yandex-team.ru>
2019-11-23 15:03:19 +0300

    Merge pull request #132 in SHRI-2019/trenders from backend.update-old-tests to master
    Squashed commit of the following:
    
    commit cdabf2d557945b5c5907ced551968f63e7637710
    Author: oleg.fry <oleg.fry@gmail.com>
    Date:   Sat Nov 23 15:02:52 2019 +0300
    
        comments_count
    
    commit 898d47b5fe79e16cd2ee823a53a74c990fd000f9
    Merge: ecdd6eb 424a4a4
    Author: oleg.fry <oleg.fry@gmail.com>
    Date:   Sat Nov 23 14:59:52 2019 +0300
    
        Merge branch 'backend.update-old-tests' of https://bb.yandex-team.ru/scm/shri-2019/trenders into backend.update-old-tests
    
    commit ecdd6eb9748a9a9094fa6d9f388c012ee9dbe598
    Author: oleg.fry <oleg.fry@gmail.com>
    Date:   Sat Nov 23 14:01:11 2019 +0300
    
        update_old tests
    
    commit 424a4a4e0221c38846627f4faa6eff4b4d11bcb8
    Author: oleg.fry <oleg.fry@gmail.com>
    Date:   Sat Nov 23 14:01:11 2019 +0300
    
        update_old tests

---
+++ backend/web/trends/models/merging.py
@@ -0,0 +1,66 @@
+from collections import defaultdict
+
+
+def group_by_title(data):
+    title_score = defaultdict(int)
+    for d in data:
+        title = d['title']
+        title_score[title] += d['day']
+    titles = set()
+    result = list()
+    for d in data:
+        title = d['title']
+        d["data"]["comments_count"] = title_score["title"]
+        if title not in titles:
+            titles.add(title)
+            result.append({'title': title, 'data': d['data'], 'day': title_score[title]})
+    return result
+
+
+def update_old(new_data, old_data):
+    """
+
+    new_data = {'movie': [{'id': 1, 'day': 1}, ]}
+    old_data = {'movie': [{'id': 1, 'day': 0}, ]}
+
+    """
+
+    old_data = {item[0]: item[1] for item in old_data.items()}
+
+    tags = set(new_data.keys()) | set(old_data.keys())
+    updated_data = {}
+    for tag in tags:
+        updated_tag_data = []
+
+        if tag not in new_data:
+            updated_data[tag] = old_data[tag]
+            continue
+
+        if tag not in old_data:
+            updated_data[tag] = new_data[tag]
+            continue
+
+        # id to trend mapping
+        old_trends = {trend['id']: trend for trend in old_data[tag]}
+        new_trends = {trend['id']: trend for trend in new_data[tag]}
+        trend_ids = set(new_trends.keys()) | set(old_trends.keys())
+
+        for trend_id in trend_ids:
+
+            if trend_id not in old_trends:
+                updated_tag_data.append(new_trends[trend_id])
+            elif trend_id not in new_trends:
+                updated_tag_data.append(old_trends[trend_id])
+            else:
+                if new_trends[trend_id]['day'] <= old_trends[trend_id]['day']:
+                    updated_tag_data.append(old_trends[trend_id])
+                else:
+                    updated_tag_data.append(new_trends[trend_id])
+
+        updated_data[tag] = updated_tag_data
+
+    updated_data = [
+        {"category": key, "data": value}
+        for key, value in updated_data.items()
+    ]
+    return updated_data
