58a4107e035311ef2fbd8d533df033796f9c2c63
Maria Isaeva <isaeva-mariya@yandex.ru>
2019-11-23 16:50:25 +0300

    deleted unused prop

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -113,7 +113,7 @@ class Carousel extends React.Component<CarouselProps, CarouselState> {
     }
 
     public render() {
-        const { title, routeUrl, carouselId, canBeHidden, tabs, children } = this.props;
+        const { title, routeUrl, carouselId, canBeHidden, tabs } = this.props;
         const { isHidden } = this.state;
 
         const carouselCn = classnames(
